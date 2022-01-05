b73fb71cd79c91a074a94448b137e6e765e891a9
ewasaputra03 <ewasaputra03@gmail.com>
2022-01-05 18:40:37 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Alarm App Using Flutter
---
+++ lib/clock/clock.dart
@@ -0,0 +1,70 @@
+import 'package:alarm_app/clock/clock_dial.dart';
+import 'package:alarm_app/clock/clock_text.dart';
+import 'package:flutter/material.dart';
+
+import 'hour_hand.dart';
+import 'minute_hand.dart';
+import 'wheel_circle_painter.dart';
+
+class Clock extends StatefulWidget {
+  const Clock({Key? key}) : super(key: key);
+
+  @override
+  _ClockState createState() => _ClockState();
+}
+
+class _ClockState extends State<Clock> with TickerProviderStateMixin {
+  double wheelSize = 400;
+  final double longNeedleHeight = 40;
+  final double shortNeedleHeight = 25;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    WheelCircle wheelCircle = WheelCircle(
+        wheelSize: wheelSize,
+        longNeedleHeight: longNeedleHeight,
+        shortNeedleHeight: shortNeedleHeight,
+        context: context);
+
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.all(40),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+          ),
+        ),
+        Stack(
+          alignment: Alignment.center,
+          children: [
+            Container(
+                color: Colors.transparent,
+                width: wheelSize,
+                height:wheelSize,
+                child: CustomPaint(
+                    painter:
+                        ClockDialPainter(clockText: ClockText.roman))),
+            Container(
+              width: wheelSize,
+              height: wheelSize,
+              color: Colors.transparent,
+              child: Center(
+                child: Container(
+                  width: 15,
+                  height: 15,
+                  decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+              ),
+            ),
+            MinuteHand(),
+            HourHand(),
+          ],
+        )
+      ],
+    );
+  }
+}
